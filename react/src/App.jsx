cab90a8c7900181db969f960049d3b7327d7f401
lyn-codeLab <annluczynski@gmail.com>
2026-03-15 11:16:39 +0000

    Update App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,8 @@
 function App() {
   return (
-    <><div className="App">
+    <div className="App">
       <h1>Hello World!</h1>
-    </div><div id="maincontent">
-        <h1>welcome</h1>
-      </div></>
+    </div>
   );
 }
 
