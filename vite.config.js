12a3c0792d88c37fa5bc60fc08caa41552b2b74c
Nashwa Hamido <nashwa.elbanna144@gmail.com>
2026-03-18 21:05:20 +0000

    added vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        groupTabs: path.resolve(__dirname, 'client/src/groupTabs.jsx'),
+      },
+      output: {
+        entryFileNames: 'js/[name].js',
+        chunkFileNames: 'js/[name].js',
+        assetFileNames: 'css/[name][extname]',
+        dir: 'public',
+      },
+    },
+  },
+});
