48dd751f14c1460056ec9df4396f2d30dee37e98
Nashwa Hamido <nashwa.elbanna144@gmail.com>
2026-03-18 21:55:01 +0000

    added itinerary.ejs

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.redirect('/itinerary/weekly'));
-
-router.get('/weekly', (req, res) => {
-  res.render('itinerary/weekly', { user: req.session.user || null });
-});
-
-router.get('/monthly', (req, res) => {
-  res.render('itinerary/monthly', { user: req.session.user || null });
+router.get('/', (req, res) => {
+  res.render('groups/itinerary', { user: req.session.user || null });
 });
 
 module.exports = router;
