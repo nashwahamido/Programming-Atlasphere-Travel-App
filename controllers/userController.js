d4dca9e8726595e56151ad989d92b613c9402e13
Nashwa Hamido <nashwa.elbanna144@gmail.com>
2026-04-19 19:54:35 +0100

    clean up process = deleted unused model and controller files

--- controllers/userController.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/profile', (req, res) => {
-  res.render('profile/setup', { user: req.session.user || null });
-});
-
-module.exports = router;
