ac0bbd25501474400061745d8d4a50fec1f34555
Nashwa Hamido <nashwa.elbanna144@gmail.com>
2026-04-20 00:32:41 +0100

    deleted extra files

--- routes/itinerary.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('itinerary/index', { user: req.session.user || null });
-});
-
-module.exports = router;
